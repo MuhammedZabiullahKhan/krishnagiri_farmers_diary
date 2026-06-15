2391046ab31a369dc555e4a17957e279daaea172
primesyssolutions <100826803+MuhammedZabiullahKhan@users.noreply.github.com>
2026-06-15 10:27:19 +0530

    Update service worker to version 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'farmers-diary-v1';
+const CACHE_NAME = 'farmers-diary-v2';
 const urlsToCache = ['/', '/index.html', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', event => {
